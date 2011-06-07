984f8f676ef6b9951c4c6513c23cac10fc46b19d
Tyler Jennings <tyle.jennings@gmail.com>
2011-06-07 11:07:26 -0500

    metaprogramming exercise content

---
+++ groupon/13a_html_dsl.rb
@@ -0,0 +1,50 @@
+class Markup
+  attr_reader :name, :children
+
+  def self.document(&block)
+    doc = new("", &block)
+    doc
+  end
+
+  def initialize(name, &block_body)
+    @children = []
+    @name = name
+
+    if block_given?
+      @text = instance_eval(&block_body)
+    end
+  end
+
+  def tag(body)
+    return body if [nil, ""].include? name
+    "<#{name}>#{body}</#{name}>"
+  end
+
+  def to_s
+    tag(@text || children.join)
+  end
+
+  def method_missing(name, &body)
+    @children << Markup.new(name, &body)
+    nil
+  end
+end
+
+#Usage
+doc = Markup.document do
+  html do 
+    title { "Foo" }
+    body do 
+      "Hello fantastic!"
+    end
+  end
+end
+puts doc
+
+#Exercises
+
+# Support block-less text body form (title "Foo")
+# Support attributes with body
+# support attribute-only tags
+#
+# Pretty formatting
